167c36abcbd702db946b8aac7d336044724bde0d
unknown <Swetha@company.com>
2022-08-27 09:53:49 +0530

    JAVA_FSD

---
+++ Phase 3/Practice project/UserManager/src/main/java/com/example/UserManager/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.UserManager.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.UserManager.entities.User;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+	public User findByName(String name);
+
+}
---
+++ Phase 3/Practice project/UserManager/src/test/java/com/example/UserManager/UserManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.UserManager;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+public class UserManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
